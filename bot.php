f4ac931ce8be0a9949e78bb7048107fbd21de000
pumlike <bank.it.net@gmail.com>
2016-12-01 03:27:32 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -10,22 +10,17 @@ if (!is_null($events['events'])) {
 	// Loop through each event
 	foreach ($events['events'] as $event) {
 		// Reply only when message sent is in 'text' format
-		curl -X POST \
--H 'Content-Type:application/json' \
--H 'Authorization: Bearer {ENTER_ACCESS_TOKEN}' \
--d '{
-    "replyToken":"nHuyWiB7yP5Zw52FIkcQobQuGDXCTA",
-    "messages":[
-        {
-            "type":"text",
-            "text":"Hello, user"
-        },
-        {
-            "type":"text",
-            "text":"May I help you?"
-        }
-    ]
-}' https://api.line.me/v2/bot/message/reply
+		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+			// Get text sent
+			$text = $event['message']['text'];
+			// Get replyToken
+			$replyToken = $event['replyToken'];
+
+			// Build message to reply back
+			$messages = [
+				'type' => 'text',
+				'text' => $text
+			];
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
