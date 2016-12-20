c256d4872d47f5600a7d6c290b5c6dff098a156b
pumlike <bank.it.net@gmail.com>
2016-12-20 22:46:44 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -2,25 +2,32 @@
 $access_token = 'CD8J9hyzGd96dJizoCZemLrIQidvUe2i+QUPhAWYDgfQOlbpKjT6XKIHbNzX2pzxVdWpJ14QZApzZ59YGxpyU/ef0hr7IKSkoQRgya5z9/Gv3cnloZUXz4tB+4d6re7ZZlu/1kT5fMnEQ4nr09blpgdB04t89/1O/w1cDnyilFU=';
 // Get POST body content
 $content = file_get_contents('php://input');
-$myfile = fopen("testfile.txt", "w");
-fwrite($myfile, $content);
-fclose($myfile);
 // Parse JSON
 $events = json_decode($content, true);
 // Validate parsed JSON data
-if (!is_null($events['events'])) {
+if (!is_null($events['events'])) 
+{
 	// Loop through each event
-	/*foreach ($events['events'] as $event) {
+	foreach ($events['events'] as $event) {
 		// Reply only when message sent is in 'text' format
-		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
+		if ($event['type'] == 'message' && $event['message']['type'] == 'text') 
+		{
 			// Get text sent
 			$text = $event['message']['text'];
 			// Get replyToken
 			$replyToken = $event['replyToken'];
+			if($text=="Hello")
+			{
+				$replytext="สวัสดี";
+			}
+			else
+			{
+				$replytext=$text;
+			}
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $text
+				'text' => $replytext
 			];
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
@@ -40,37 +47,6 @@ if (!is_null($events['events'])) {
 			curl_close($ch);
 			echo $result . "\r\n";
 		}
-	}*/
-}
-else
-{
-	$job=$_GET["job"];
-	if($job=="job01")
-	{
-		//$replytext="ตอบคุณ ".$sourceInfo['displayName']."\n";];
-		$pushtext="ได้รับการแจ้งซ่อม Case NO. ".$_GET["case"]." จาก ".$_GET["user1"]." อุปกรณ์ ".$_GET["equip"];
-		$messages = [
-						'type' => 'text',
-						'text' =>  $pushtext
-					];
-		// Make a POST Request to Messaging API to reply to sender
-		$url = 'https://api.line.me/v2/bot/message/push';
-		$data = [
-			'to' => 'C5284c7dfc515bb45cbb338ae3be11356',
-			'messages' => [$messages],
-		];
-		$post = json_encode($data);
-		$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-		$ch = curl_init($url);
-		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-		$result = curl_exec($ch);
-		curl_close($ch);
-		echo $result . "\r\n";
 	}
 }
 echo "OK";
-?> 
