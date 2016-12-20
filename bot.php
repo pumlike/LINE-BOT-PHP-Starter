57f5dc673b83b99bb34cbc5140ecb5684c8b236c
pumlike <bank.it.net@gmail.com>
2016-12-20 22:52:35 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -7,7 +7,11 @@ $events = json_decode($content, true);
 // Validate parsed JSON data
 if (!is_null($events['events'])) 
 {
-	
+	// Loop through each event
+	foreach ($events['events'] as $event) {
+		// Reply only when message sent is in 'text' format
+		if ($event['type'] == 'message' && $event['message']['type'] == 'text') 
+		{
 			// Get text sent
 			$text = $event['message']['text'];
 			// Get replyToken
@@ -20,11 +24,7 @@ if (!is_null($events['events']))
 			{
 				$replytext=$text;
 			}
-			// Build message to reply back
-			$messages = [
-				'type' => 'text',
-				'text' => $replytext
-			];
+			
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
