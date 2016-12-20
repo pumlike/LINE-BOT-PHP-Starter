42d54b1427cfc6861ff71dd6b2da35093e20f78b
pumlike <bank.it.net@gmail.com>
2016-12-20 23:01:35 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -3,7 +3,7 @@ $access_token = 'CD8J9hyzGd96dJizoCZemLrIQidvUe2i+QUPhAWYDgfQOlbpKjT6XKIHbNzX2pz
 // Get POST body content
 $content = file_get_contents('php://input');
 // Parse JSON
-$events = json_decode($content, false);
+$events = json_decode($content, true);
 // Validate parsed JSON data
 if (!is_null($events['events'])) 
 {
