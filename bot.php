c406455b92bee59aa4c33eff4e293f37764cc79d
pumlike <bank.it.net@gmail.com>
2016-12-20 23:03:38 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -4,9 +4,6 @@ $access_token = 'CD8J9hyzGd96dJizoCZemLrIQidvUe2i+QUPhAWYDgfQOlbpKjT6XKIHbNzX2pz
 $content = file_get_contents('php://input');
 // Parse JSON
 $events = json_decode($content, true);
-// Validate parsed JSON data
-if (!is_null($events['events'])) 
-{
 	// Loop through each event
 	foreach ($events['events'] as $event) {
 		// Reply only when message sent is in 'text' format
