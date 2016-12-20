3b848f577848fbb677d6033d9e6b2fc83e127dbe
pumlike <bank.it.net@gmail.com>
2016-12-20 22:33:03 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -16,7 +16,7 @@ if (!is_null($events['events'])) {
 			$replyToken = $event['replyToken'];
 			// Build message to reply back
 			$messages = [
-				'type' => 'text',
+				'ดี' => 'สวัสดี',
 				'text' => $text
 			];
 			// Make a POST Request to Messaging API to reply to sender
